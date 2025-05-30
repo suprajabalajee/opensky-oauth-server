6dbf22840feb7eba3e1261e8da3e5dfe3ea4ca63
suprajabalajee <supraja.balajee@gmail.com>
2025-05-30 22:09:45 +0530

    render changes

--- server/src/oauth_server.js
+++ server/src/oauth_server.js
@@ -1,7 +1,6 @@
-require('dotenv').config();
-
 const express = require('express');
 const cors = require('cors');
+const fetch = require('node-fetch');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -9,19 +8,17 @@ const PORT = process.env.PORT || 3001;
 const CLIENT_ID = process.env.CLIENT_ID;
 const CLIENT_SECRET = process.env.CLIENT_SECRET;
 
-
 app.use(cors());
 
 app.get('/token', async (req, res) => {
   try {
-    const fetch = (await import('node-fetch')).default;
     const response = await fetch('https://auth.opensky-network.org/auth/realms/opensky-network/protocol/openid-connect/token', {
       method: 'POST',
       headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
       body: new URLSearchParams({
         grant_type: 'client_credentials',
-        client_id: process.env.CLIENT_ID,
-        client_secret: process.env.CLIENT_SECRET
+        client_id: CLIENT_ID,
+        client_secret: CLIENT_SECRET
       })
     });
 
